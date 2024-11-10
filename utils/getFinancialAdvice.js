f6c25cb29e0c8a42eccfb45a13dc806081657e57
Ayush Singh ☁️ <ayush1337@hotmail.com>
2024-11-10 19:56:51 +0530

    🔨: Update financial advice API key and adjust currency context in getFinancialAdvice function

--- utils/getFinancialAdvice.js
+++ utils/getFinancialAdvice.js
@@ -5,14 +5,14 @@ const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 const getFinancialAdvice = async (totalBudget, totalIncome, totalSpend) => {
   console.log(totalBudget, totalIncome, totalSpend);
   try {
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
 
     const prompt = `
       Based on the following financial data:
-      - The total buget I have prepared is ${totalBudget} USD 
-      - Out of this allocated budget I have spent ${totalSpend} USD 
-      - My total income is ${totalIncome} USD
-      Provide me financial advice in 4 sentences to help me manage my finances more effectively.
+      - The total buget I have prepared is ${totalBudget} INR 
+      - Out of this allocated budget I have spent ${totalSpend} INR 
+      - My total income is ${totalIncome} INR
+      Provide me financial advice in 4 sentences to help me manage my finances more effectively in Indian context.
     `;
 
     const result = await model.generateContent(prompt);
