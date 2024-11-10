93def2e6f4fe01a1162efb78ca84c097ee98e9bd
Ayush Singh ☁️ <ayush1337@hotmail.com>
2024-11-10 19:51:06 +0530

    🔨: Update database credentials to use environment variables

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,9 +3,7 @@ export default {
   schema: './utils/schema.jsx',
   out: './drizzle',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:ischm0qSJOI3@ep-still-poetry-a1v8iq5f.ap-southeast-1.aws.neon.tech/kooberdb?sslmode=require',
-    connectionString:
-      'postgresql://neondb_owner:ischm0qSJOI3@ep-still-poetry-a1v8iq5f.ap-southeast-1.aws.neon.tech/kooberdb?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 };
-  
